a92b9010ea265947c1259fc29a335e882627916e
mauraaninditakirana <kiranamaura351@gmail.com>
2024-04-05 13:49:46 +0700

    menggunakan perulangan while dalam prosedur input

--- Algoritmasearch.cpp
+++ Algoritmasearch.cpp
@@ -7,5 +7,10 @@ int i; //index of array element
 
 void input()
 {
-    
+    while (true)
+    {
+        cout << "Enter the number of elements in the array: ";
+        cin>> n;
+        
+    }
 }
